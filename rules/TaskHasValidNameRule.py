f80f38ef2ec00193bc71ac48774f80bf17722019
Guido Grazioli <ggraziol@redhat.com>
2022-03-03 11:01:47 +0100

    Add verbs to valid task names

--- rules/TaskHasValidNameRule.py
+++ rules/TaskHasValidNameRule.py
@@ -35,11 +35,11 @@ DESC: str = r"""Rule to test if files are smalll enough.
 
 VERBS: typing.List[str] = """\
 add ask assemble become begin call check collect configure copy create debug
-delete deploy determine disable do download drop enable ensure execute exit
-extract fail find flag generate get go install help keep leave let link load
-look make move notify own parse perform play put refresh reload remove reown
-retrieve restart run set show start stop take talk tell test try turn update
-use validate verify wait work\
+delete deploy determine disable display do download drop enable ensure execute
+exit extract fail find flag generate get go install help keep leave let link
+load look make move notify own parse perform play put read refresh reload
+remove reown retrieve restart run set show start stop take talk tell test try
+turn update use validate verify wait work\
 """.split()
 
 VERBS_ALL: typing.List[str] = VERBS + [v.capitalize() for v in VERBS]
